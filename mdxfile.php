1c16f78f47517a3495f00f23c55123ade345bac6
fain182 <fain182@gmail.com>
2017-05-21 17:01:30 +0200

    add code coverage on codeclimate

--- mdxfile.php
+++ mdxfile.php
@@ -3,3 +3,8 @@
 function test(\Mudephix\Context $context) {
     $context->local('./vendor/bin/phpunit tests ');
 }
+
+function testOnCI(\Mudephix\Context $context) {
+    $context->local('./vendor/bin/phpunit --coverage-clover build/logs/clover.xml tests ');
+    $context->local('./vendor/bin/test-reporter'); // report coverage to codeclimate
+}
